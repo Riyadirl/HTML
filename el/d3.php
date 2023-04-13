6bbcf19c0efb55e547651bffb3061f8a675ee97f
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-04-14 02:36:46 +0600

    .

--- el/d3.php
+++ el/d3.php
@@ -2,12 +2,13 @@
 $sql = "SELECT id, name, email FROM users";
 $result = mysqli_query($conn, $sql);
 
+
 if (mysqli_num_rows($result) > 0) {
     // Output data of each row
     while($row = mysqli_fetch_assoc($result)) {
         echo "id: " . $row["id"] . " - Name: " . $row["name"] . " - Email: " . $row["email"] . "<br>";
     }
-    
+
 } else {
     echo "0 results";
 }
